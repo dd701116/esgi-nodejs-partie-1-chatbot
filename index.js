ea13795cb346f7bfff89877ad34cd34ecb57b008
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-03-22 15:02:10 +0100

    add fb bot endpoint

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const {initialization, bonjour, bonjour_x, chat} = require('./functions')
-const app = express()
+const {FacebookMessagingAPIClient, ValidateWebhook, FacebookMessageParser} = require('fb-messenger-bot-api')
+
+
 const PORT = process.env.PORT
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+
+const messagingClient = new facebook.FacebookMessagingAPIClient(PAGE_ACCESS_TOKEN);
+const app = express()
 
 app.use(express.json())
 
@@ -12,5 +18,23 @@ app.get("/hello", bonjour_x)
 
 // TODO: ajouter le point d'entrée `POST /chat` comme spécifié dans l'énoncé
 app.post("/chat", chat)
+
+//  TODO: FB messenger bot
+app.get('/api/webhook',ValidateWebhook.validateServer)
+app.post('/api/webhook', (req, res) => {
+  try {
+    const incomingMessages = messageParser.parsePayload(req.body)
+        await messagingClient.markSeen(senderId)
+        await messagingClient.toggleTyping(senderId,true)
+
+        //promise based reaction on message send confirmation
+        const result = await messagingClient.sendTextMessage(senderId, 'Hello');
+        console.log(`Result sent with: ${result}`)
+    } catch(e){
+      console.log(e)
+    }
+    //callback based reaction on message confirmation
+    messagingClient.sendTextMessage("TESTID", 'Hello',(result) => console.log(`Result sent with: ${result}`))
+})
 
 app.listen(PORT, () => initialization(PORT))
