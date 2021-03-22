f9c516766a8b0ec0dd73e0b40d3f4b4b4e3b595d
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-03-22 12:45:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const {initialization, bonjour, bonjour_x, chat} = require('./functions')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.use(express.json())
 
